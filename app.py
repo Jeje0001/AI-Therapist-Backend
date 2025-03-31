191de23b7d4ca684de8712f0f1ca71eb43e6e089
jejea <157323284+Jeje0001@users.noreply.github.com>
2025-03-30 23:07:21 -0400

    FIrst changes

---
+++ app.py
@@ -0,0 +1,127 @@
+import sqlite3
+import hashlib
+import datetime
+from flask import Flask,request,jsonify,session
+
+app=Flask(__name__)
+app.secret_key="AyomideTherapist2025!xK9p"
+
+def initialize_database():
+    conn=sqlite3.connect("therapist.db")
+    c=conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, 
+              username TEXT UNIQUE, 
+              email TEXT UNIQUE, 
+              password_hash TEXT, 
+              subscription_status TEXT DEFAULT 'free')''')
+    c.execute('''CREATE TABLE IF NOT EXISTS  chats (id INTEGER PRIMARY KEY, 
+              user_id INTEGER, 
+              user_message TEXT, 
+              response TEXT, 
+              timestamp TEXT)''')
+    conn.commit()
+    conn.close()
+
+
+@app.route("/signup",methods=["POST"])
+
+def signup():
+    data=request.get_json()
+
+    if not data or "username" not in data or "email" not in data or "password" not in data:
+        return jsonify({"response":"Missing fields"}) 
+    
+    username=data["username"]
+    email=data["email"]
+    password=data["password"]
+
+    password_hash=hashlib.sha256(password.encode()).hexdigest()
+    conn=sqlite3.connect("therapist.db")
+    c=conn.cursor()
+    try:
+        c.execute('''INSERT INTO users (username, email, password_hash) 
+                VALUES (?, ?, ?)''', (username, email, password_hash))
+    except sqlite3.IntegrityError:
+         conn.close()
+         return jsonify({"response": "Email or username taken"})
+    
+    conn.commit()
+    conn.close()
+    return jsonify({"response":"signed up Successfully"})
+
+@app.route("/login",methods=["POST"])
+
+def login():
+    data=request.get_json()
+
+    if not data or "email" not in data or "password" not in data:
+        return jsonify({"response":"Missing fields"})
+    
+    email=data["email"]
+    password=data["password"]
+
+    password_hash= hashlib.sha256(password.encode()).hexdigest()
+    conn=sqlite3.connect("therapist.db")
+    c=conn.cursor()
+    c.execute("SELECT id, password_hash FROM users WHERE email = ?", (email,))
+
+    user=c.fetchone()
+
+    if user and user[1] == password_hash:
+        session["user_id"]=user[0]
+        conn.close()
+        return jsonify({"response":"Logged in"})
+    else:
+        conn.close()
+        return jsonify({"response":"Invalid login"})
+    
+
+@app.route("/chat",methods=["POST"])
+
+def chat():
+
+    if "user_id" not in session:
+        return jsonify({"response": "Login first!"})
+    user_message=request.get_json()
+
+    if not user_message or "text" not in user_message.keys():
+        return jsonify({"response":"No message"})
+    user_message=user_message["text"]
+
+    if not user_message:
+        return  jsonify({"response":"No message"})
+    
+    conn=sqlite3.connect("therapist.db")
+    c=conn.cursor()
+    try:
+        c.execute('''INSERT INTO chats
+                (user_id, user_message, response, timestamp) 
+                VALUES (?, ?, ?, ?)''', 
+                (session["user_id"], user_message, "I’m here for you", datetime.datetime.now().isoformat()))
+    except sqlite3.OperationalError:
+        return jsonify({"response": "Try again later"})
+        
+    conn.commit()
+    conn.close()
+    return jsonify({"response":"I am here for you"})
+
+
+@app.route("/logout",methods=["POST"])
+
+def logout():
+    session.clear()
+    return jsonify({"response":"Logged out"})
+
+@app.route("/status",methods=["GET"])
+
+def status():
+    if "user_id" in session:
+        return jsonify({"response":"Logged in","user_id":session["user_id"]})
+    else:
+        return jsonify({"response":"Not logged in"})
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
